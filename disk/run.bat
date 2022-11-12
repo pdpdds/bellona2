dc96151b62f712c0dabce021f43f37bade837df5
Juhang Park <juhang3@daum.net>
2022-11-13 04:54:19 +0900

    user shell

--- disk/run.bat
+++ disk/run.bat
@@ -1,1 +1,1 @@
-Multiboot.exe launcher.exe bellona2.bin guiapp.mod gui.mod stdlib.mod jpeg.mod wall.jpg wall.bmp login.exe clock.exe tt.exe wall.exe
+Multiboot.exe launcher.exe bellona2.bin guiapp.mod gui.mod stdlib.mod jpeg.mod wall.jpg wall.bmp login.exe clock.exe tt.exe wall.exe ush.exe
